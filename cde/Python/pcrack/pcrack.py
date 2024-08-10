bc3d9f18a78ea611588b14a181475aaa3fe85362
e-t <76783922+etisHere@users.noreply.github.com>
2024-08-10 10:05:30 +0000

    Speed update pcrack.py
    Speed update :
    
    -now will know pass attempts and won't try them again 
    -added output of attempt count

--- cde/Python/pcrack/pcrack.py
+++ cde/Python/pcrack/pcrack.py
@@ -5,7 +5,10 @@ import sys
 
 
 #This small script will take input at launch and try to guess what you put as input randomly.
- 
+
+pass_at = set()
+
+attempts = 0
 
 def crack(passw):
     passw = f"{passw}"
@@ -19,38 +22,58 @@ def crack(passw):
 
 #Slow just prints off anything that it attempts, this will really slow things down..Hence the name! 
     def slow():
+        pass_at = set()
         start_time = time.time()
-        pchoice = ""
-        while pchoice != passw:
-            random_cho = random.randint(int(start_range), int(end_range))
-            cchar =  string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
-            pchoice = ''.join(random.choices(cchar, k=random_cho))
-            if pchoice == passw:
-                end_time = time.time()
-                t_output = end_time - start_time
-                print("Password is: \n------------")
-                print(pchoice)
-                print("------------\n Took %.2f seconds to figure out!.. \n \n [This would be faster in fast mode js!]" % t_output)
-                break
-            else:
-                print(pchoice)
-
-
-
-    def fast():
-        start_time = time.time()
+        attempts = 0
         pchoice = ""
         while pchoice != passw:
             random_cho = random.randint(int(start_range), int(end_range))
             #Remove any string from here(cchar) or on the slow speed to speed up things(this is just the type of printable characters)
             cchar =  string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
             pchoice = ''.join(random.choices(cchar, k=random_cho))
+            if pchoice in pass_at:               
+                pass 
+            elif pchoice not in pass_at:
+                pass_at.add(pchoice)
+                attempts += 1
+                #continue
             if pchoice == passw:
                 end_time = time.time()
                 t_output = end_time - start_time
                 print("Password is: \n------------")
                 print(pchoice)
                 print("------------\n Took %.2f seconds to figure out!" % t_output)
+                print("Also took this many attempts..", attempts)
+                #print(pass_at)
+                break
+            else:
+                print(pchoice)
+
+
+    def fast():
+        pass_at = set()
+        start_time = time.time()
+        attempts = 0
+        pchoice = ""
+        while pchoice != passw:
+            random_cho = random.randint(int(start_range), int(end_range))
+            #Remove any string from here(cchar) or on the slow speed to speed up things(this is just the type of printable characters)
+            cchar =  string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
+            pchoice = ''.join(random.choices(cchar, k=random_cho))
+            if pchoice in pass_at:               
+                pass 
+            elif pchoice not in pass_at:
+                pass_at.add(pchoice)
+                attempts += 1
+                #continue
+            if pchoice == passw:
+                end_time = time.time()
+                t_output = end_time - start_time
+                print("Password is: \n------------")
+                print(pchoice)
+                print("------------\n Took %.2f seconds to figure out!" % t_output)
+                print("Also took this many attempts..", attempts)
+                #print(pass_at)
                 break
 
 
